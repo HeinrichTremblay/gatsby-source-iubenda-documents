02b55064d19b5ab6a99cbb3bb037b4626e9f499b
Heinrich Tremblay <heinrich.tremblay@outlook.com>
2021-04-01 17:54:25 +0800

    feat: Multiple documents support (#5)
    BREAKING CHANGE: Change to the plugin API

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,53 +1,95 @@
 const fetch = require("node-fetch");
 
-exports.onPreInit = () =>
-  console.log("✨ Loaded gatsby-source-iubenda-documents");
-
 const PRIVACY_URL = "https://www.iubenda.com/api/privacy-policy/";
 const TERMS_URL = "https://www.iubenda.com/api/terms-and-conditions/";
 
+exports.onPreInit = () =>
+  console.log("✨ Loaded gatsby-source-iubenda-documents");
+
 exports.sourceNodes = async (
-  { actions, createContentDigest, createNodeId },
+  { actions, createContentDigest, createNodeId, getNodesByType },
   pluginOptions
 ) => {
   const { createNode } = actions;
-  const { documentId } = pluginOptions;
+  const { documentIds } = pluginOptions;
+
+  const formatResult = (content) =>
+    content.replace(/"/g, "").replace(/\\/g, "");
 
   const fetchDocument = async (url) => {
-    const res = await fetch(`${url}/no-markup`);
-    const json = await res.json();
-    return json.content.replace(/"/g, "").replace(/\\/g, "");
+    const response = await fetch(`${url}/no-markup`);
+
+    if (!response.ok) {
+      const error = new Error();
+      error.code = response.status;
+      throw error;
+    }
+
+    const json = await response.json();
+    return formatResult(json.content);
   };
 
-  const data = {};
+  const handleError = (error, name, documentId) => {
+    // Error codes from https://www.iubenda.com/en/help/78-privacy-policy-direct-text-embedding-api#api
 
-  try {
-    const document = await fetchDocument(PRIVACY_URL + documentId);
-    data["privacyPolicy"] = document;
-    console.log("🚛  Fetched privacy policy.");
-  } catch (error) {
-    console.log("⚠️  No privacy policy");
-  }
+    switch (error.code) {
+      case 404:
+        console.log(`👀 ${name} for ${documentId} not found.`);
+        break;
+      case 403:
+        console.log(
+          `🛑 To access the ${name} for ${documentId} via API, convert it to Pro.`
+        );
+        break;
+      case 500:
+        console.log(
+          "❌ Application Error. Please contact info@iubenda.com for support."
+        );
+        break;
+      default:
+        console.log("❌ An unexpected error happened!");
+        break;
+    }
+  };
 
-  try {
-    const document = await fetchDocument(TERMS_URL + documentId);
-    data["termsAndConditions"] = document;
-    console.log("🚛  Fetched terms and conditions.");
-  } catch (error) {
-    console.log("⚠️  No terms and conditions");
-  }
+  const logFetchSuccess = (name, documentId) => {
+    console.log(`🚛 ${name} for ${documentId} fetched.`);
+  };
 
-  createNode({
-    ...data,
-    id: createNodeId(documentId),
-    documentId: documentId,
-    parent: null,
-    children: [],
-    internal: {
-      type: "IubendaDocument",
-      contentDigest: createContentDigest(data),
-    },
-  });
+  return await Promise.all(
+    documentIds.map(async (documentId) => {
+      const data = {
+        privacyPolicy: null,
+        termsAndConditions: null,
+      };
 
-  return;
+      try {
+        const document = await fetchDocument(PRIVACY_URL + documentId);
+        data.privacyPolicy = document;
+        logFetchSuccess("Privacy Policy", documentId);
+      } catch (error) {
+        handleError(error, "Privacy Policy", documentId);
+      }
+
+      try {
+        const document = await fetchDocument(TERMS_URL + documentId);
+        data.termsAndConditions = document;
+        logFetchSuccess("Terms and Conditions", documentId);
+      } catch (error) {
+        handleError(error, "Terms and Conditions", documentId);
+      }
+
+      createNode({
+        ...data,
+        id: createNodeId(documentId),
+        documentId: documentId,
+        parent: null,
+        children: [],
+        internal: {
+          type: "IubendaDocument",
+          contentDigest: createContentDigest(data),
+        },
+      });
+    })
+  );
 };
